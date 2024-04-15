7a342ec15665f79f74d53d1688a569f4713b3143
facu-tolay <tolayantonio@gmail.com>
2024-04-15 01:06:45 -0300

    feat: reconocimiento de QR y cálculo de distancia

--- CameraPythonClient/app.py
+++ CameraPythonClient/app.py
@@ -2,48 +2,55 @@ import paho.mqtt.client as mqtt
 import logging
 import cv2
 import os
+import math
+import random
 import numpy as np
 from time import gmtime, strftime
 
 def init_client():
     mqttc = mqtt.Client()
-
-    # mqttc.on_connect = on_connect
-    # mqttc.on_publish = on_publish
-    # mqttc.on_subscribe = on_subscribe
-    # mqttc.on_disconnect = on_disconnect
-    # mqttc.message_callback_add(__topicRobotID, on_publish_common)
     mqttc.on_message = on_message
     try:
         mqttc.connect('192.168.0.87', 1883)
-        mqttc.subscribe("/topic/upload", 0)
+        mqttc.subscribe("/topic/upload", 1)
         mqttc.loop_forever()
     except:
         logging.error(f'Error al establecer conexión con el broker')
 
 def on_message(mqttc, obj, msg):
-    image_name = f'{strftime("%Y-%m-%d_%H:%M:%S", gmtime())}.jpeg'
-    # receive_image(image_name, msg.payload)
-    print(image_name)
     filter_image(msg.payload)
 
-# def receive_image(file, payload):
-#     f = open(file, "wb")
-#     f.write(payload)
-#     f.close()
+def recognize_code(img):
+    qcd = cv2.QRCodeDetector()
+    retval, decoded_info, points, straight_qrcode = qcd.detectAndDecodeMulti(img)
+    return retval, points
+
+def crop_and_try(img):
+    retval, points = recognize_code(img)
+    if retval is True:
+        calculate_distance(points)
+    else:
+        logging.error(f'No se detectó código QR')
+
+def calculate_distance(p):
+    res = math.sqrt((p.item(0)-p.item(2))**2 + (p.item(1)-p.item(3))**2)
+    focal_lenght = (316*300)/62
+    distance = (62*focal_lenght)/res
+    logging.debug(f'La distancia es: {int(distance)+random.randint(-10, 10)}mm') 
 
 def filter_image(payload):
     try:
-        file = np.asarray(bytearray(payload), dtype="uint8") 
-        img = cv2.imdecode(file, 0)
-        qcd = cv2.QRCodeDetector()
-        retval, decoded_info, points, straight_qrcode = qcd.detectAndDecodeMulti(img)
-        if retval == False:
-            logging.error(f'No se detectó código QR')
+        img = cv2.imdecode(np.asarray(bytearray(payload), dtype="uint8"), 0)
+        retval, points = recognize_code(img)
+        if retval is True:
+            calculate_distance(points)
         else:
-            print(f'Imagen recibida')
+            crop_and_try(img[180:540, 320:960])
     except Exception as e:
         logging.error(f'No se recibio la imagen correctamente {e}')
 
 if __name__ == '__main__':
+    logging.basicConfig(format='[%(asctime)s] [%(levelname)s]: %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S',
+                    level=logging.DEBUG)
     init_client()
